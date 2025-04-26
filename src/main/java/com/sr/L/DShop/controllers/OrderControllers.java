be9777031f2b0a8ae987cf17be485171b5d819ff
Sudikshan <rsudikshan2@gmail.com>
2025-04-26 16:14:34 +0545

    fix: missing controller

--- src/main/java/com/sr/L/DShop/controllers/OrderControllers.java
+++ src/main/java/com/sr/L/DShop/controllers/OrderControllers.java
@@ -2,16 +2,22 @@ package com.sr.L.DShop.controllers;
 
 import com.sr.L.DShop.annotations.OrderController;
 import com.sr.L.DShop.payload.Request.OrderRequest;
+import com.sr.L.DShop.service.OrderService;
+import com.sr.L.DShop.service.impl.OrderServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @OrderController
+@RequiredArgsConstructor
 public class OrderControllers {
+
+    private final OrderServiceImpl orderService;
 
     @PostMapping("/setOrder")
     public ResponseEntity<?> setOrder(@RequestBody OrderRequest orderRequest){
 
-        return null;
+        return ResponseEntity.ok().body(orderService.setOrder(orderRequest));
     }
 }
--- src/main/java/com/sr/L/DShop/entities/Payment.java
+++ src/main/java/com/sr/L/DShop/entities/Payment.java
@@ -1,4 +1,5 @@
 package com.sr.L.DShop.entities;
 
 public class Payment {
+    //need external api then after only we can design this schema
 }
