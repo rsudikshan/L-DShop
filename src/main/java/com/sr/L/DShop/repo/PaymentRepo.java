80f042c3c44a088a0876d1cc52f01df9cbf75b46
Sudikshan <rsudikshan2@gmail.com>
2025-04-27 13:05:57 +0545

    fix: missing annotation

--- src/main/java/com/sr/L/DShop/repo/PaymentRepo.java
+++ src/main/java/com/sr/L/DShop/repo/PaymentRepo.java
@@ -2,6 +2,8 @@ package com.sr.L.DShop.repo;
 
 import com.sr.L.DShop.entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PaymentRepo extends JpaRepository<Payment,Long> {
 }
